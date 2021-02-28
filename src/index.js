92be29a8210229619053f4d251506ab1f417afff
Erin O'Connor <48839566+oconnorir@users.noreply.github.com>
2021-02-27 22:01:12 -0800

    added auth components (login, register) and routes to app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
 import './index.css';
 import App from './components/App';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+
+
 import reportWebVitals from './reportWebVitals';
+
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route exact path='/' component={App} />
+      <Route path='/login' component={Login} />
+      <Route path='/register' component={Register} />
+    </Switch>
+  </Router>
+)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
