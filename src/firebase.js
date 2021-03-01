8a590d2d75da3479af4e669c582b5c2698565956
Erin O'Connor <48839566+oconnorir@users.noreply.github.com>
2021-02-28 21:36:33 -0800

    added firebase config file (firebase.js) and add app to firebase

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/storage';
+
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCUG0_VNskVphZYb-Bf_EQ6f5KcghvsuAI",
+    authDomain: "react-slack-56d3d.firebaseapp.com",
+    projectId: "react-slack-56d3d",
+    storageBucket: "react-slack-56d3d.appspot.com",
+    messagingSenderId: "727305501288",
+    appId: "1:727305501288:web:6d144615667905bad659dd",
+    measurementId: "G-C1HVNZ5Z42"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+
+export default firebase;
